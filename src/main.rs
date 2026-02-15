3c13e804132315e31c29850ac40081b4c1a989c9
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-02-15 11:06:23 +0900

    Support input from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
-use std::{env, fs};
+use std::{env, fs, io};
+use std::io::Read;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = env::args().skip(1);
@@ -9,14 +10,20 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn run(mut args: impl Iterator<Item = String>) -> Result<(), Box<dyn Error>> {
-    let file = args.next().ok_or("File path required")?;
+    let path = args.next();
 
     if args.next().is_some() {
         return Err("Too many arguments".into());
     }
 
-    let content = fs::read_to_string(file)?;
-    println!("{}", content);
+    let mut reader: Box<dyn Read> = match path.as_deref() {
+        None | Some("-") => Box::new(io::stdin()),
+        Some(path) => Box::new(fs::File::open(path)?),
+    };
 
+    let mut content = String::new();
+    reader.read_to_string(&mut content)?;
+
+    println!("{}", content);
     Ok(())
 }
