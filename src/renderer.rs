2abbc218a22ed2c1d6419c9ba0b956ae1f031e48
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-02-22 14:51:39 +0900

    Add workflow title to Mermaid output

--- src/renderer.rs
+++ src/renderer.rs
@@ -8,9 +8,13 @@ pub struct MermaidFlowchart;
 
 impl Renderer for MermaidFlowchart {
     fn render(&self, document: &ArazzoDocument) -> String {
-        let mut output = String::from("flowchart TD\n");
+        let mut output = String::new();
 
         for workflow in &document.workflows {
+            output.push_str(&title(&workflow));
+
+            output.push_str("flowchart TD\n");
+
             for pair in workflow.steps.windows(2) {
                 let from = &pair[0];
                 let to = &pair[1];
@@ -28,6 +32,10 @@ impl Renderer for MermaidFlowchart {
     }
 }
 
+fn title(workflow: &Workflow) -> String {
+    format!("---\ntitle: {}\n---\n", workflow.workflow_id)
+}
+
 fn node_label(step: &Step) -> String {
     step.description.as_ref().map_or(String::from(""), |v| format!("[{}]", v))
 }
@@ -40,6 +48,7 @@ mod tests {
     fn render_steps() {
         let arazzo = ArazzoDocument {
             workflows: vec![Workflow {
+                workflow_id: String::from("workflow_a"),
                 steps: vec![
                     Step {
                         step_id: String::from("step_foo"),
@@ -63,6 +72,9 @@ mod tests {
         let actual = sut.render(&arazzo);
 
         let expected = concat!(
+            "---\n",
+            "title: workflow_a\n",
+            "---\n",
             "flowchart TD\n",
             "    step_foo[description_foo] --> step_bar[description_bar]\n",
             "    step_bar[description_bar] --> step_baz[description_baz]\n",
@@ -75,6 +87,7 @@ mod tests {
     fn render_steps_without_description() {
         let arazzo = ArazzoDocument {
             workflows: vec![Workflow {
+                workflow_id: String::from("workflow_a"),
                 steps: vec![
                     Step {
                         step_id: String::from("step_foo"),
@@ -94,6 +107,9 @@ mod tests {
         let actual = sut.render(&arazzo);
 
         let expected = concat!(
+            "---\n",
+            "title: workflow_a\n",
+            "---\n",
             "flowchart TD\n",
             "    step_foo --> step_bar\n",
         );
